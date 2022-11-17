aa57a32d63a0bca5b8a440584982eae9b15a2a5f
HP <HP@10.12.20.94>
2022-11-17 15:14:13 +0530

    hello

---
+++ src/jUnitTestPackage/testAddNumbers.java
@@ -0,0 +1,15 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitFunction junit = new jUnitFunction();
+		int result = junit.addNum(100, 200);
+		assertEquals (300, result);
+	}
+}
